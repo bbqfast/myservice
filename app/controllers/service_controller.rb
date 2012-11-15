c36201b953243407066ecd1ea7d6e9ce3b75d790
bbqnow@hotmail.com <bbqnow@hotmail.com>
2012-11-14 23:32:12 -0800

    add dropdown list

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -1,13 +1,21 @@
-class LoginController < ApplicationController
+class ServiceController < ApplicationController
 
-def index
+def new
+  @customers = Customer.all
+  @submitters = Submitter.all
   
-  
-  
+    respond_to do |format|
+      format.html # index.html.erb
+      #format.json { render json: @posts }
+    end  
 end
 
-  
+def testdata
+  @c = Customer.create(:name => 'April')
+  @c = Customer.create(:name => 'John')
+  @c.save
+end
   
 
    
-end
+end 
